25ae7bbfeb067203e1e73148c9984be8b5d21b6d
Henrique <henriquesg09@hotmail.com>
2022-06-11 19:29:52 -0300

    generate paymentDueDate dynamically

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -186,7 +186,6 @@ export default {
       invoiceDate: null,
       paymentTerms: null,
       paymentDueDateUnix: null,
-      paymentDueDate: null,
       productDescription: null,
       invoicePending: null,
       paymentTermsOptions: [
@@ -204,12 +203,30 @@ export default {
       invoiceTotal: 0,
     };
   },
+  created() {
+    this.invoiceDateUnix = Date.now();
+    this.invoiceDate = new Date(this.invoiceDateUnix).toLocaleDateString(
+      "en-us",
+      this.dateOptions
+    );
+  },
   methods: {
     ...mapMutations(["TOGGLE_INVOICE"]),
     closeInvoice() {
       this.TOGGLE_INVOICE();
 
       return;
+    },
+  },
+  watch: {
+    paymentTerms() {
+      const futureDate = new Date();
+      this.paymentDueDateUnix = futureDate.setDate(
+        futureDate.getDate() + parseInt(this.paymentTerms)
+      );
+      this.paymentDueDate = new Date(
+        this.paymentDueDateUnix
+      ).toLocaleDateString("en-us", this.dateOptions);
     },
   },
 };
